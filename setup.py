040855f691054a11deca49b7df6162e174ea48dc
Nit-code-droid <nitikatyagi596@gmail.com>
2024-08-31 20:21:39 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+hyphen_E_DOT='-e.'
+def get_requirements(file_path:str)->List[str]:
+    # list of requirements
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if hyphen_E_DOT in requirements:
+            requirements.remove(hyphen_E_DOT)
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Nitika',
+    author_email='nitikatyagi596@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
+
